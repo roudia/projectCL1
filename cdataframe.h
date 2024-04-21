4ea9ca7b83cc8afbaf8743682f71ff3ab076989d
roudia <judia.ouazari@efrei.net>
2024-04-21 19:30:08 +0200

    division of the information in different files

---
+++ cdataframe.h
@@ -0,0 +1,31 @@
+
+typedef struct
+{
+    int ls;
+    int ps;
+    int rows;
+    COLUMN *data;
+}CDATAFRAME;
+
+CDATAFRAME *createEmptyDataframe();
+int insert_column(CDATAFRAME *, COLUMN *);
+void hardFillDataframe(CDATAFRAME *);
+void display_all_cdata(CDATAFRAME *);
+void display_part_rows(CDATAFRAME *,int ,int);
+void display_part_col(CDATAFRAME *,int,int);
+
+void add_rows_val(CDATAFRAME *,int);
+void delete_rows_val(CDATAFRAME*,int);
+void add_col_cdata(CDATAFRAME *,int);
+void delete_col_val(CDATAFRAME*,int);
+int exist_val(CDATAFRAME*,int );
+void access_cell(CDATAFRAME*,int ,int );
+void replace_cell(CDATAFRAME*,int ,int );
+void rename_title(CDATAFRAME *,int);
+
+void display_name(CDATAFRAME *);
+void num_rows(CDATAFRAME *);
+void num_col(CDATAFRAME *);
+void num_equal(CDATAFRAME*,int );
+void num_great(CDATAFRAME*,int );
+void num_lower(CDATAFRAME*,int );
